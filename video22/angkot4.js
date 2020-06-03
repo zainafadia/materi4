60b7312c9468ba63a5d7e3b2f6f5834847954ebe
zainafadia <64583032+zainafadia@users.noreply.github.com>
2020-06-03 21:03:30 +0800

    Pengkondisian If Else

---
+++ video22/angkot4.js
@@ -0,0 +1,14 @@
+var jmlAngkot = 10;
+var angkotBeroperasi = 6;
+var noAngkot = 1;
+
+
+for(noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++){
+	if (noAngkot <= 6) {
+		console.log("Angkot No. " + noAngkot + " beroperasi dengan baik");
+	} else if (noAngkot === 8) {
+		console.log("Angkot No. " + noAngkot + " sedang lembur");
+	} else {
+		console.log("Angkot No. " + noAngkot + " sedang tidak beroperasi");
+	}
+}
