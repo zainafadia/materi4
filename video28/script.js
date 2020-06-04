b7be1a9b6282e0c3e8cc1a94f2b571e7a3bbb58d
zainafadia <64583032+zainafadia@users.noreply.github.com>
2020-06-04 21:05:22 +0800

    function

---
+++ video28/script.js
@@ -0,0 +1,17 @@
+var a;
+var b;
+
+function jumlahVolume(a, b){
+var volumeA;
+var volumeB;
+var total;
+
+	volumeA = math.pow(a, 3);
+	volumeB = math.pow(b, 3);
+
+	total = volumeA + volumeB;
+
+	return total; 
+}
+
+jumlahVolume(a, b);
