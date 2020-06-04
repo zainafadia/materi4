72cc6527f2530428b0624b0035fbd22ed47a2e3a
zainafadia <64583032+zainafadia@users.noreply.github.com>
2020-06-04 21:55:38 +0800

    refactoring

---
+++ video30/script.js
@@ -0,0 +1,17 @@
+
+function jumlahVolumeDuaKubus(a, b){
+
+var volumeA;
+var volumeB;
+var total;
+
+volumeA = a * a * a;
+volumeB = b * b * b;
+
+total = volumeA + volumeB;
+
+return total;
+
+}
+
+alert(jumlahVolumeDuaKubus(8,3));
