53519365ae63cbe0ae1333d51e5bb7968a3ea808
zainafadia <64583032+zainafadia@users.noreply.github.com>
2020-06-05 21:36:33 +0800

    Slice & Splice

---
+++ video36/script.js
@@ -0,0 +1,12 @@
+// var arr = ['fadia', 'laila', 'hidayati'];
+
+// //splice
+// //splice(indexAwal, mauHapusBerapa, elemenBaru, elemenBaru, ..)
+// arr.splice(0,0, 'zaina');
+// console.log(arr.join(' - '));
+
+//5. Slice
+// Slice(awal, akhir);
+var arr = ['zaina','fadia', 'laila', 'hidayati'];
+var arr2 = arr.slice(0,3);
+console.log(arr2.join(' - '));
