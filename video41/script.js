03ff828fb59995e6807bbc3069af7a3c90ef08c3
zainafadia <64583032+zainafadia@users.noreply.github.com>
2020-06-05 22:55:38 +0800

    Object

---
+++ video41/script.js
@@ -0,0 +1,11 @@
+//membuat Object
+var mahasiwa = {
+	nama : "Fadia Laila",
+	umur : 19,
+	ips : [3.49, 3.20, 3.57, 3.78, 3.90],
+	alamat :{
+		jalan : "Ir. PM. Noor",
+		kota : "Banjarbaru",
+		provinsi : "Kalimantan Selatan"
+	}
+};
